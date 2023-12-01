d945b60299d163b9fe7983e46bc6ea1fb6ee4e78
Ganesh-16N <ganunagargoje16@gmail.com>
2023-12-01 17:22:15 +0530

    backend completed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
  
 const app = express();
 const PORT = process.env.PORT || 3000;
-const mongoURI = `${process.env.URI}`;
+const mongoURI =process.env.URI;
 
 const client = new MongoClient(mongoURI, {
   tls: true, 
