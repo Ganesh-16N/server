6b8c038c3565d43426f573e30a4666dc30ee1f1e
Ganesh-16N <ganunagargoje16@gmail.com>
2023-12-01 11:02:00 +0530

    commited

---
+++ controllers/userController.js
@@ -0,0 +1,64 @@
+const User = require('../models/userModel');
+
+async function createUser(req, res) {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+}
+
+async function getAllUsers(req, res) {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+async function getUserById(req, res) {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+async function updateUserById(req, res) {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+}
+
+async function deleteUserById(req, res) {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+module.exports = {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUserById,
+  deleteUserById,
+};
---
+++ index.js
@@ -0,0 +1,38 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const mongoURI = "mongodb+srv://ganesh:Ganu123@cluster0.h721f7t.mongodb.net/your-database-name?retryWrites=true&w=majority";
+
+const client = new MongoClient(mongoURI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function connectToMongoDB() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    await mongoose.connect(mongoURI);
+
+    console.log("Connected to MongoDB using Mongoose");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+connectToMongoDB();
+app.use(express.json());
+
+app.use('/', userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
